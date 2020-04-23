433bce3e824049df35a8b7b2a60488fb429c2856
cododev <59783538+cododev@users.noreply.github.com>
2020-04-23 04:06:46 +0100

    HelpDesk System
    HelpDesk System

--- users/includes/footer.php
+++
@@ -1,7 +0,0 @@
- <footer class="">
-          <div class="text-center">
-              <hr>
-              <img src="../img/aau.png" width="300">
-              <p> &copy; 2019 - AMBROSE ALLI UNIVERSITY</p>
-          </div>
-      </footer>

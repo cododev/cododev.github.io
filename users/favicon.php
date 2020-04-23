b90f00f54dc1de5822aba45a7515985c0baaa24f
cododev <59783538+cododev@users.noreply.github.com>
2020-04-23 03:31:00 +0100

    Add files via upload

---
+++ users/favicon.php
@@ -0,0 +1,11 @@
+ 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="Dashboard">
+  <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+<link rel="shortcut icon" type="image/x-icon" href="../img/favicon.png" />
+</head>
